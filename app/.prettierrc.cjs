35be1716c077aa16ff36a340b43d0c8d421cc906
gabriel-picoli <vnvsgabe@gmail.com>
2025-08-16 17:03:54 -0300

    chore: create react.ts + swc project

---
+++ app/.prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false, // remove ; no final das linhas
+  singleQuote: true, // usa aspas simples em vez de duplas
+  printWidth: 100, // quebra de linha quando passar de 100 colunas
+  tabWidth: 2, // identação com 2 espaços
+  trailingComma: 'none', // remove virgula no último item em objetos/arrays
+  endOfLine: 'auto' // ajusta automaticamente para o sistema operacional
+}

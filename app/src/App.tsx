4c581a904374912aba5a2dd9a4ab0fe5be1d0762
gabriel-picoli <vnvsgabe@gmail.com>
2025-08-18 20:07:25 -0300

    feat: add theme and global style

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+
+import theme from './styles/theme'
+import GlobalStyle from './styles/GlobalStyle'
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+
       <div>alo teste alo</div>
-    </>
+    </ThemeProvider>
   )
 }
 

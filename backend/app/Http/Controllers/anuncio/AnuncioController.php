fb8fd2c933df3a379b39bcce82f395eb74b74cff
Mateusparca <mateusparcianello2@gmail.com>
2025-08-21 21:39:20 -0300

    feat: criação de produtos

---
+++ backend/app/Http/Controllers/anuncio/AnuncioController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\anuncio;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AnuncioController extends Controller
+{
+    //
+}
---
+++ backend/app/Models/Produto.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    use HasFactory;
+
+    protected $table = 'produtos';
+
+    protected $fillable = [
+        'name',
+        'tipo',
+        'conservacao',
+        'genero',
+        'artista',
+        'quanti',
+        'capa',
+        'lancamento',
+        'id_user',
+    ];
+}

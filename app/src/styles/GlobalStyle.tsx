8a22492ae84222efd106581e5a91e47c1f0d8381
gabriel-picoli <vnvsgabe@gmail.com>
2025-08-23 15:29:16 -0300

    feat: created initial project layout

--- app/src/styles/GlobalStyle.tsx
+++ app/src/styles/GlobalStyle.tsx
@@ -2,11 +2,11 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
     * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      font-family: 'Newsreader', serif;
-    }
+     margin: 0;
+     padding: 0;
+     box-sizing: border-box;
+     font-family: 'Poppins', sans-serif;
+   }
 
 `
 

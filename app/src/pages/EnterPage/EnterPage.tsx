bb209236469deb1840a2c35441c877f2630275ca
gabriel-picoli <vnvsgabe@gmail.com>
2025-08-23 12:37:20 -0300

    feat: created input component

--- app/src/pages/EnterPage/EnterPage.tsx
+++ app/src/pages/EnterPage/EnterPage.tsx
@@ -1,3 +1,46 @@
+import { useForm } from 'react-hook-form'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import z from 'zod'
+
+import Input from '../../components/Input/Input'
+
+const schema = z.object({
+  name: z.string().min(2, 'Name must be at least 2 characters long'),
+  lastName: z.string().min(2, 'Last Name must be at least 2 characters long')
+})
+
+type DataProps = z.infer<typeof schema>
+
 export default function EnterPage() {
-  return <div>home page</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<DataProps>({
+    mode: 'onBlur',
+    resolver: zodResolver(schema)
+  })
+
+  return (
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <Input
+        type="text"
+        {...register('name')}
+        placeholder="Name"
+        label="Name"
+        helperText={errors.name?.message}
+      />
+
+      <Input
+        type="text"
+        {...register('lastName')}
+        placeholder="Last Name"
+        label="Last Name"
+        helperText={errors.lastName?.message}
+      />
+
+      <button type="submit">Enter</button>
+    </form>
+  )
 }

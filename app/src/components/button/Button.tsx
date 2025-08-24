b45e2bf8bae5a8425de70681b9df32395fdb02a9
gabriel-picoli <vnvsgabe@gmail.com>
2025-08-24 14:40:02 -0300

    feat: created button variants

--- app/src/components/button/Button.tsx
+++ app/src/components/button/Button.tsx
@@ -1,14 +1,56 @@
 import * as S from './styles'
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+interface BaseButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode
-  type: 'button' | 'submit' | 'reset'
+  size?: 'small' | 'medium' | 'large'
 }
 
-export default function Button({ children, type, ...props }: ButtonProps) {
+interface ButtonProps extends BaseButtonProps {
+  variant?: 'primary' | 'outline' | 'rounded-outline' | 'cancel'
+}
+
+function Button({ children, variant = 'primary', size = 'medium', type, ...props }: ButtonProps) {
   return (
-    <S.Button type={type} {...props}>
+    <S.Button type={type} $variant={variant} $size={size} {...props}>
       {children}
     </S.Button>
   )
 }
+
+Button.Primary = ({ children, size = 'medium', ...props }: BaseButtonProps) => (
+  <Button variant="primary" size={size} {...props}>
+    {children}
+  </Button>
+)
+
+Button.Outline = ({ children, size = 'medium', ...props }: BaseButtonProps) => (
+  <Button variant="outline" size={size} {...props}>
+    {children}
+  </Button>
+)
+
+Button.RoundedOutline = ({ children, size = 'medium', ...props }: BaseButtonProps) => (
+  <Button variant="rounded-outline" size={size} {...props}>
+    {children}
+  </Button>
+)
+
+Button.Cancel = ({ children, size = 'medium', ...props }: BaseButtonProps) => (
+  <Button variant="cancel" size={size} {...props}>
+    {children}
+  </Button>
+)
+
+Button.Small = ({ children, variant = 'primary', ...props }: Omit<ButtonProps, 'size'>) => (
+  <Button variant={variant} size="small" {...props}>
+    {children}
+  </Button>
+)
+
+Button.Large = ({ children, variant = 'primary', ...props }: Omit<ButtonProps, 'size'>) => (
+  <Button variant={variant} size="large" {...props}>
+    {children}
+  </Button>
+)
+
+export default Button
--- app/src/components/button/styles.tsx
+++ app/src/components/button/styles.tsx
@@ -1,21 +1,90 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import theme from '../../styles/theme'
 
-export const Button = styled.button`
-  background-color: ${theme.colors.primary};
-  border: none;
-  color: white;
-  padding: 15px 32px;
+interface ButtonProps {
+  $variant: 'primary' | 'outline' | 'rounded-outline' | 'cancel'
+  $size: 'small' | 'medium' | 'large'
+}
+
+const buttonVariants = {
+  primary: css`
+    background-color: ${theme.colors.primary};
+    border: none;
+    color: white;
+
+    &:hover {
+      background-color: ${theme.colors.primaryHover};
+    }
+  `,
+  outline: css`
+    background-color: transparent;
+    border: 2px solid ${theme.colors.black};
+    color: ${theme.colors.black};
+
+    &:hover {
+      background-color: ${theme.colors.black};
+      color: white;
+    }
+  `,
+  'rounded-outline': css`
+    background-color: transparent;
+    border: 2px solid ${theme.colors.black};
+    border-radius: 25px;
+    color: ${theme.colors.black};
+
+    &:hover {
+      background-color: ${theme.colors.black};
+      color: white;
+    }
+  `,
+  cancel: css`
+    background-color: ${theme.colors.red};
+    border: none;
+    color: white;
+
+    &:hover {
+      background-color: ${theme.colors.redHover};
+    }
+  `
+}
+
+const buttonSizes = {
+  small: css`
+    padding: 8px 16px;
+    font-size: 14px;
+  `,
+  medium: css`
+    padding: 15px 32px;
+    font-size: 16px;
+  `,
+  large: css`
+    padding: 20px 40px;
+    font-size: 18px;
+  `
+}
+
+export const Button = styled.button<ButtonProps>`
   text-align: center;
   text-decoration: none;
   display: inline-block;
-  font-size: 16px;
   margin: 4px 2px;
   cursor: pointer;
   transition: all 0.3s ease;
+  border-radius: 2px;
+  font-weight: 500;
 
-  &:hover {
-    background-color: ${theme.colors.primaryHover};
+  ${({ $variant }) => buttonVariants[$variant]}
+
+  ${({ $size }) => buttonSizes[$size]}
+  
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+
+    &:hover {
+      background-color: initial;
+      color: initial;
+    }
   }
 `

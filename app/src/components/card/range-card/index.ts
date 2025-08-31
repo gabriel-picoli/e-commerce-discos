60a9628a595bd2488f50422fe0851435e22864c2
gabriel-picoli <vnvsgabe@gmail.com>
2025-08-31 17:02:04 -0300

    feat: BRUHHHHH

--- app/src/components/card/range-card/index.ts
+++
@@ -1,9 +0,0 @@
-import { RangeCardRoot, RangeCardDescription, RangeCardImage } from './RangeCard'
-
-const RangeCard = {
-  Root: RangeCardRoot,
-  Image: RangeCardImage,
-  Description: RangeCardDescription
-}
-
-export default RangeCard
--- app/src/pages/Main/styles.tsx
+++ app/src/pages/Main/styles.tsx
@@ -8,11 +8,3 @@ export const Main = styled.main`
   flex-direction: column;
   position: relative;
 `
-
-export const RangeCardContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 1rem;
-  padding: 2rem;
-`

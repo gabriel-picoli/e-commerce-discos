53513526884fc31c1ddbcc021b67c4c74049d616
gabriel-picoli <vnvsgabe@gmail.com>
2025-09-09 20:15:11 -0300

    style: add title and description

--- app/src/pages/Main/styles.tsx
+++ app/src/pages/Main/styles.tsx
@@ -8,3 +8,22 @@ export const Main = styled.main`
   flex-direction: column;
   position: relative;
 `
+
+export const Title = styled.h1`
+  font-size: 4.8rem;
+  font-weight: 900;
+  color: ${theme.colors.neutral_900};
+  text-align: center;
+  margin: 3rem 0 1.5rem 0;
+  line-height: 1.1;
+`
+
+export const Description = styled.p`
+  font-size: 1.5rem;
+  color: ${theme.colors.neutral_900};
+  font-weight: 300;
+  letter-spacing: 1.5px;
+  margin-bottom: 3rem;
+  text-align: center;
+  z-index: 1;
+`
